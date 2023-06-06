9fb6bc4ab09d43d9cbe9701aa74bde9644a1ea17
Atharva Gupta <atharva.gu@gmail.com>
2023-06-05 19:30:53 -0500

    Finish layout of the wordle game
    Basic implementation of the game. Update further

--- wordle_plus.js
+++ wordle_plus.js
@@ -8,7 +8,7 @@ var row = 0;
 var col = 0;
 
 var gameOver = false;
-var word = "SQUID"
+var word = "Audio";
 
 window.onload = function() {
     initialize();
@@ -23,6 +23,59 @@ function initialize() {
             tile.classList.add("tile-box");
             tile.innerText = "";
             document.getElementById("board").appendChild(tile);
+        }
+    }
+
+    // Listen for Key Press
+    document.addEventListener("keyup", (key) => {
+        if (gameOver) {
+            return ;
+        }
+        if ("KeyA" <= key.code && key.code <= "KeyZ") {
+            if (col < wordLength) {
+                let currTile = document.getElementById(row.toString() + "-" + col.toString());
+                if (currTile.innerText == "") {
+                    currTile.innerText = key.code[3];
+                    col++;
+                }
+            }
+        } else if (key.code == "Backspace") {
+            if (col > 0 && col <= wordLength) {
+                col--;
+            }
+            let currTile = document.getElementById(row.toString() + "-" + col.toString());
+            currTile.innerText = "";    
+
+        } else if (key.code == "Enter") {
+            update();
+            //start a new row
+            row += 1;
+            col = 0;
+        }
+        if (!gameOver && row == guesses) {
+            gameOver = true;
+            document.getElementById("answer").innerText = word;
+        }
+    })
+}
+
+function update() {
+    let correct = 0;
+    for (let i = 0; i < wordLength; i++) {
+        let tile = document.getElementById(row.toString() + "-" + i.toString());
+        let letter = tile.innerText;
+        
+        if (word[i] == letter) {
+            tile.classList.add("answer-correct");
+            correct++;
+        } else if (word.includes(letter)) {
+            tile.classList.add("partially-correct");
+        } else {
+            tile.classList.add("incorrect");
+        }
+
+        if (correct == wordLength) {
+            gameOver = true;
         }
     }
 }
